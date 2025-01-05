3a4a01f6ae541f2cc32a280ee621b169f5a25435
sivanandhini953 <sivanandhinis69@gmail.com>
2025-01-05 11:58:55 +0530

    Create q21.v

---
+++ Verilog/Assignments/Assign2_struct_model/q21.v
@@ -0,0 +1,14 @@
+module or_switch(A,B,Y);
+input A,B;
+output Y;
+supply0 gnd;
+supply1 vdd;
+wire a;
+pmos p1(a,vdd,A);
+pmos p2(Y,a,B);
+nmos n1(Y,gnd,A);
+nmos n2(Y,gnd,B);
+endmodule
+
+
+Ans   nor gate
