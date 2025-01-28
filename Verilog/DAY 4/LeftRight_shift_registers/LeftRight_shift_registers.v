c7c43353b378cfb8443eeadff3f66c240cffdbfc
sivanandhini953 <sivanandhinis69@gmail.com>
2025-01-28 17:32:16 +0530

    Update LeftRight_shift_registers.v

--- Verilog/DAY 4/LeftRight_shift_registers/LeftRight_shift_registers.v
+++ Verilog/DAY 4/LeftRight_shift_registers/LeftRight_shift_registers.v
@@ -25,3 +25,36 @@ parameter WIDTH=4;
 	
 		
 endmodule
+
+
+ALTERNATE:
+
+module shift_reg(
+    input d,
+    input  clk,
+    input  reset,
+    output reg [WIDTH-1:0]q,
+	input left,
+	input right
+    
+);
+parameter WIDTH=4;
+
+
+    always @(posedge clk or negedge reset) begin
+        if (!reset)
+            q <= 0;
+			
+			
+        else if (right)begin
+		q<=q>>1;
+		q[3]<=d;
+		end
+		else if (left)begin
+		q<=q<<1;
+		q[0]<=d;
+		end
+		end
+	
+		
+endmodule
