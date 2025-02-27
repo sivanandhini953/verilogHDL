3ca9b645929cc1369b12d69d480c3c1a73f829e0
sivanandhini953 <sivanandhinis69@gmail.com>
2025-02-27 16:53:15 +0530

    Create MORE THAN 1 1'S IN LAST THREE SEQUENCE TB.v

---
+++ Verilog/Assignments/ASSIGN 5_FSM/MORE THAN 1 1'S IN LAST THREE SEQUENCE TB.v
@@ -0,0 +1,47 @@
+module tb_moore_111_detector;
+    reg clk, reset, x;
+    wire y;
+
+   
+    fsm uut (
+        .clk(clk),
+        .reset(reset),
+        .x(x),
+        .y(y)
+    );
+
+    
+    always #5 clk = ~clk;
+	initial begin
+$dumpfile ("fsm1.vcd");
+$dumpvars;
+end
+
+    initial begin
+       
+        clk = 0;
+        reset = 1;
+        
+
+       
+        #10 reset = 0;x=1;
+
+        
+        #10 x = 1;
+        #10 x = 0;
+        #10 x = 0;
+        #10 x = 1;
+		#10 x = 0;
+		#10 x = 1;		
+
+        
+     
+        #20 $finish;
+    end
+
+  
+    initial begin
+        $monitor("Time = %0t | Input = %b | y = %b", $time, x,y);
+    end
+
+endmodule
